b65420a0b292a3a2e726a7cba43f741763384f7b
ryo-i <iidazz0109@gmail.com>
2026-01-25 22:45:50 +0900

    ALTER TABLE文の基本構文追加

--- setup/migrations/02_add_columns_to_users.sql
+++ setup/migrations/02_add_columns_to_users.sql
@@ -1,3 +1,17 @@
+-- ALTER TABLE文の基本構文
+
+-- 【カラム追加】
+-- ALTER TABLE [table_name]
+--   ADD COLUMN [column_name] [type] [constraint];
+
+-- 【カラム削除】
+-- ALTER TABLE [table_name]
+--   DROP COLUMN [column_name];
+
+-- 【カラム型変更】
+-- ALTER TABLE [table_name]
+--   ALTER COLUMN [column_name] TYPE [new_type];
+
 -- 既存カラムの型変更や制約変更
 ALTER TABLE users ALTER COLUMN email TYPE VARCHAR(100);
 
