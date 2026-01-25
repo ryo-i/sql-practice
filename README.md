a3926a16928d6c7e5df51f1d2ead7aa389248785
ryo-i <iidazz0109@gmail.com>
2026-01-25 15:43:43 +0900

    テーブルを作成

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 このリポジトリは、SQLを理解するために
 PostgreSQL + psql を使って学習した記録を残すものです。
 
+## 学習ステップ
+
+- [x] PostgreSQL / psql のセットアップ
+- [x] データベース作成・接続
+- [x] テーブル作成（DDL）
+- [ ] INSERT（データ投入）
+- [ ] CRUD（単一テーブル）
+- [ ] テーブル定義の変更（ALTER TABLE）
+- [ ] 既存レコードの補完・修正（UPDATE）
+- [ ] 複数テーブル設計（外部キー）
+- [ ] JOIN
+- [ ] 集計・GROUP BY
+- [ ] インデックス & 実行計画
+
 ## 前提
 
 - PostgreSQL がローカルにインストールされている
@@ -12,41 +26,67 @@ PostgreSQL + psql を使って学習した記録を残すものです。
 
 インストール確認
 ```bash
-psql --version
+$ psql --version
 ```
 
 （PostgreSQLが入っていない場合）
 ```bash
-brew install postgresql
+$ brew install postgresql
 ```
 
 起動確認
 ```bach
-pg_isready
+$ pg_isready
 ```
 （起動してない場合）
 ```
-brew services start postgresql
+$ brew services start postgresql
 ```
 
 ## 起動手順
 
 DB作成
 ```bash
-createdb sql_practice
+sql_practice=# createdb sql_practice
 ```
 
 psql起動
 ```bash
-psql sql_practice
+sql_practice=# psql sql_practice
 ```
 
-psql終了
-```back
-\q
+（psql終了）
+```bash
+sql_practice=# \q
 ```
 
 ## 構成
 
 - setup/: DB・テーブル定義
 - basic/: SQLの基礎
+
+## テーブル作成
+
+テーブル作成
+```bash
+sql_practice=# \i setup/migrations/01_create_tables.sql
+```
+
+テーブル一覧
+```bash
+sql_practice=# \dt
+```
+
+テーブル定義
+```bash
+sql_practice=# \d users
+```
+
+### 最新のDBスキーマをダンプする
+
+migrationsに段階的にDBを定義していくため、DBの最新版のスキーマを取得する
+
+（psqlを開いている場合は一度閉じてターミナルで実行）
+```bash
+$ pg_dump -s sql_practice > setup/snapshot/schema_latest_dump.sql
+```
---
+++ setup/snapshot/schema_latest_dump.sql
@@ -0,0 +1,89 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict UQ1mfdxj1nYLxIICD4Lx6hBmZ0wqDgk88FzqaWv0ZDMNYmhsW0TMmEw3GZdi2eM
+
+-- Dumped from database version 14.20 (Homebrew)
+-- Dumped by pg_dump version 14.20 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: iidaryou
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
+ALTER TABLE public.users OWNER TO iidaryou;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: iidaryou
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO iidaryou;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: iidaryou
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: iidaryou
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: iidaryou
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: iidaryou
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict UQ1mfdxj1nYLxIICD4Lx6hBmZ0wqDgk88FzqaWv0ZDMNYmhsW0TMmEw3GZdi2eM
+
