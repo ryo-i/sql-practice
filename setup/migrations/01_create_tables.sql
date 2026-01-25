97a0b049f874d0b7fbe0ce0f2b8ab393365dffa6
ryo-i <iidazz0109@gmail.com>
2026-01-25 17:55:10 +0900

    CREATE TABLE文構成のコメント追加

--- setup/migrations/01_create_tables.sql
+++ setup/migrations/01_create_tables.sql
@@ -1,3 +1,10 @@
+-- CREATE TABLE文の基本構文
+-- CREATE TABLE [table_name] (
+--   [column1] [type1] [constraint],
+--   [column2] [type2] [constraint],
+--   ...
+-- );
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
